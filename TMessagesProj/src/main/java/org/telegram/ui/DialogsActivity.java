8fefe1bfa78343ae91f8c77042fda0e54c95c69c
23rd <23rd@vivaldi.net>
2018-10-16 23:57:03 +0300

    Changed title of main view.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -1650,6 +1650,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             if (folderId == 0) {
                 actionBar.setSupportsHolidayImage(true);
             }
+            actionBar.setTitle("Fork Client");
         }
         if (!onlySelect) {
             actionBar.setAddToContainer(false);
