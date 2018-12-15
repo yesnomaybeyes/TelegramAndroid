004317bd4c78f413626011062438a05988d2c9a0
23rd <23rd@vivaldi.net>
2018-12-15 21:19:22 +0300

    Changed color of notification icon.

--- TMessagesProj/src/main/java/org/telegram/messenger/NotificationsController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/NotificationsController.java
@@ -2879,7 +2879,7 @@ public class NotificationsController extends BaseController {
                     .setGroupSummary(true)
                     .setShowWhen(true)
                     .setWhen(((long) lastMessageObject.messageOwner.date) * 1000)
-                    .setColor(0xff11acfa);
+                    .setColor(0xff3A2E5C);
 
             long[] vibrationPattern = null;
             int importance = 0;
@@ -3620,7 +3620,7 @@ public class NotificationsController extends BaseController {
                     .setContentText(text.toString())
                     .setAutoCancel(true)
                     .setNumber(messageObjects.size())
-                    .setColor(0xff11acfa)
+                    .setColor(0xff3A2E5C)
                     .setGroupSummary(false)
                     .setWhen(date)
                     .setShowWhen(true)
