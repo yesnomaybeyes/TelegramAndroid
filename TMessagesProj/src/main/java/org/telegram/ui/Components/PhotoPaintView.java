305dd2561a74dcdafd7714d1595e0bd3a0f674d1
thermatk <thermatk@thermatk.com>
2020-01-03 10:51:34 +0100

    [TF][KILL] Google Vision

--- TMessagesProj/src/main/java/org/telegram/ui/Components/PhotoPaintView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/PhotoPaintView.java
@@ -24,10 +24,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.face.Face;
-import com.google.android.gms.vision.face.FaceDetector;
-
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.Bitmaps;
 import org.telegram.messenger.BuildVars;
@@ -44,7 +40,7 @@ import org.telegram.ui.ActionBar.ActionBar;
 import org.telegram.ui.ActionBar.ActionBarPopupWindow;
 import org.telegram.ui.ActionBar.AlertDialog;
 import org.telegram.ui.ActionBar.Theme;
-import org.telegram.ui.Components.Paint.PhotoFace;
+import org.telegram.ui.Components.Paint.Views.EditTextOutline;
 import org.telegram.ui.Components.Paint.Views.EntitiesContainerView;
 import org.telegram.ui.Components.Paint.Views.EntityView;
 import org.telegram.ui.Components.Paint.Views.StickerView;
@@ -111,7 +107,7 @@ public class PhotoPaintView extends FrameLayout implements EntityView.EntityView
     private Animator colorPickerAnimator;
 
     private DispatchQueue queue;
-    private ArrayList<PhotoFace> faces;
+    //private ArrayList<PhotoFace> faces;
 
     private boolean ignoreLayout;
 
@@ -447,7 +443,7 @@ public class PhotoPaintView extends FrameLayout implements EntityView.EntityView
         entitiesView.setVisibility(VISIBLE);
         renderView.setVisibility(View.VISIBLE);
         if (facesBitmap != null) {
-            detectFaces();
+            //detectFaces();
         }
     }
 
@@ -1358,6 +1354,7 @@ public class PhotoPaintView extends FrameLayout implements EntityView.EntityView
         popupWindow.startAnimation();
     }
 
+    /*
     private int getFrameRotation() {
         switch (originalBitmapRotation) {
             case 90: {
@@ -1425,6 +1422,7 @@ public class PhotoPaintView extends FrameLayout implements EntityView.EntityView
             }
         });
     }
+    */
 
     private StickerPosition calculateStickerPosition(TLRPC.Document document) {
         TLRPC.TL_maskCoords maskCoords = null;
@@ -1438,15 +1436,16 @@ public class PhotoPaintView extends FrameLayout implements EntityView.EntityView
         }
 
         StickerPosition defaultPosition = new StickerPosition(centerPositionForEntity(), 0.75f, 0.0f);
+        /*
         if (maskCoords == null || faces == null || faces.size() == 0) {
             return defaultPosition;
         } else {
             int anchor = maskCoords.n;
 
             PhotoFace face = getRandomFaceWithVacantAnchor(anchor, document.id, maskCoords);
-            if (face == null) {
+            if (face == null) {*/
                 return defaultPosition;
-            }
+            }/*
 
             Point referencePoint = face.getPointForAnchor(anchor);
             float referenceWidth = face.getWidthForAnchor(anchor);
@@ -1517,7 +1516,7 @@ public class PhotoPaintView extends FrameLayout implements EntityView.EntityView
 
         return false;
     }
-
+	*/
     private static class StickerPosition {
         private Point position;
         private float scale;
